0f497e9c38fbf0d8099c442fd774f81f2c0df5d1
Uri Livshits <uri.livshits@gmail.com>
2021-08-28 14:26:59 +0300

    added geolocation support, added bing maps with autosearch and polygon functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from "react";
+import Map from "./Map";
 
 function App() {
+  //adding browser geolocation logic for use in maps
+  const [defaultCoords, setDefaultCoords] = useState([]);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          setDefaultCoords(position.coords);
+          console.log("coords fetched");
+        },
+        error => {
+          if (error.PERMISSION_DENIED) {
+            console.log("position denied");
+          }
+          else if (error.POSITION_UNAVAILABLE) {
+            console.log("position unavailable");
+          }
+          else if (error.TIMEOUT) {
+            console.log("position timeout");
+          }
+          else if (error.UNKNOWN_ERROR) {
+            console.log("unknown error");
+          }
+        });
+    }
+    else if (!navigator.geolocation) {
+      console.log("coords not supported by the browser");
+      return;
+    }
+  }, []);
+  // console.log(defaultCoords);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        defaultCoords.length !== 0 ? 
+        <Map defaultCoords={defaultCoords}/>
+        :
+        <p>Please enable geolocation access in your browser and reload the page.</p>
+      }
     </div>
   );
 }
---
+++ src/Map.js
@@ -0,0 +1,97 @@
+import React, {useEffect, useState} from "react";
+import {myApi} from "./myApi";
+
+const Map = ({defaultCoords}) => {
+    
+    const [mapSaved, setMapSaved] = useState([]);
+
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = `https://www.bing.com/api/maps/mapcontrol?callback=GetMap&key=${myApi.key}`;
+        script.async = true;
+        script.defer = true;
+        document.body.appendChild(script);
+        
+        let pinsCounter = 0;
+        let coords = [];
+
+        const loadMapScenario = () => {
+           //launching basic functionality centered at ISP location - may be off depending on your ISP
+            var map = new window.Microsoft.Maps.Map(document.getElementById('myMap'), {
+                center: new window.Microsoft.Maps.Location(defaultCoords.latitude, defaultCoords.longitude),
+                zoom: 9
+           });
+
+           //adding autosuggest - suggestion pins also include the polygon functionality
+           window.Microsoft.Maps.loadModule('Microsoft.Maps.AutoSuggest', function () {
+            var options = {
+                maxResults: 4,
+                map: map
+            };
+            var manager = new window.Microsoft.Maps.AutosuggestManager(options);
+            manager.attachAutosuggest('#searchBox', '#searchBoxContainer', selectedSuggestion);
+            });
+
+            function selectedSuggestion(suggestionResult) {
+                if (pinsCounter >= 3) {
+                    map.entities.clear(); //would've added instead .shift() functionality to remove only the oldest marker from the map but need more time to figure out how to manipulate map.entities
+                    coords = [];
+                    pinsCounter = 0;
+                };
+                // map.setView({ bounds: suggestionResult.bestView });
+                map.setView({ center: new window.Microsoft.Maps.Location(suggestionResult.location.latitude, suggestionResult.location.longitude), zoom: 10 });
+                var pushpin = new window.Microsoft.Maps.Pushpin(suggestionResult.location);
+                map.entities.push(pushpin);
+                document.getElementById('printoutPanel').innerHTML =
+                '<br> ' + suggestionResult.formattedSuggestion +
+                '<br> Lat: ' + suggestionResult.location.latitude +
+                '<br> Lon: ' + suggestionResult.location.longitude;
+                pinsCounter++;
+                //saving results for triangulation
+                suggestionResult.timeStamp = Date.now();
+                setMapSaved(oldArray => [...oldArray, suggestionResult] );
+                
+                //adding polygon functionality
+                coords.push(new window.Microsoft.Maps.Location(suggestionResult.location.latitude, suggestionResult.location.longitude));
+                
+                var polygon = new window.Microsoft.Maps.Polygon(coords, {
+                    fillColor: 'rgba(0, 255, 0, 0.5)',
+                    strokeColor: 'red',
+                    strokeThickness: 2
+                });
+        
+                map.entities.push(polygon);
+            };
+
+        };
+
+        //couldn't work out a better way to launch the native maps so far in the given timeframe; existing npm packages lack the required functionality though
+        setTimeout(() => {
+            loadMapScenario();
+        }, 500);
+
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, [defaultCoords.latitude, defaultCoords.longitude]);
+    
+    if (mapSaved.length > 3) {
+        let fullArray = mapSaved;
+        // fullArray.shift();
+        setMapSaved([fullArray[fullArray.length - 1]]);
+    };
+
+    // console.log("mapSaved", mapSaved);
+    
+    return (
+        <div>
+            <div style={{zIndex: "2", position: "absolute"}} id='searchBoxContainer'><input type= 'text' id= 'searchBox'/></div>
+            <div style={{zIndex: "1", position: "absolute", top: "1.5rem"}} id='printoutPanel'></div>
+            <div  style={{zIndex: "-1", position: "absolute"}} id="myMap"></div>
+        </div>
+    )
+};
+
+export default Map;
+
